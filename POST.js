72ce815013eb33a5a9a2cfe147a168ed7241e20d
Yiteng Huang <hyy528924543@gmail.com>
2021-09-17 09:28:21 +1000

    combine GET and POST method

--- POST.js
+++ POST.js
@@ -5,10 +5,11 @@ const server = http.createServer((req, res) => {
   if (req.method === "POST") {
     console.log("req content-type: ", req.headers["content-type"]);
     let postData = "";
+    // 当接收数据时触发
     req.on("data", (chunk) => {
       postData += chunk.toString();
     });
-
+    // 当接收结束时触发
     req.on("end", () => {
       console.log("postData: ", postData);
       res.end("hello world");
