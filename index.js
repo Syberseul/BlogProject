67fa7ca12b26c941c29363d876a4a8a803f8eefd
Yiteng Huang <hyy528924543@gmail.com>
2021-09-17 10:00:15 +1000

    test router

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-const http = require("http");
-const querystring = require("querystring");
+const handleBlogRouter = require("./src/router/blog");
+const handleUserRouter = require("./src/router/user");
 
-const server = http.createServer((req, res) => {
-  const method = req.method;
-  const url = req.url;
-  const path = url.split("?")[0];
-  const query = querystring.parse(url.split("?")[1]);
+const serverHandle = (req, res) => {
+  // 设置返回格式 JSON
+  res.setHeader("Content-type", "application/json");
 
-  // 设置返回格式为 JSON
-  res.setHeader("Content-type", "application/json"); // 默认返回值为'text/html'
-
-  // 返回的数据
-  const resData = {
-    method,
-    url,
-    path,
-    query,
-  };
-
-  // 返回
-  if (method === "GET") {
-    res.end(JSON.stringify(resData));
+  // 处理 blog 路由
+  const blogData = handleBlogRouter(req, res);
+  if (blogData) {
+    res.end(JSON.stringify(blogData));
+    return;
   }
-  if (method === "POST") {
-    let postData = "";
-    req.on("data", (chunk) => {
-      postData += chunk.toString();
-    });
-    req.on("end", () => {
-      resData.postData = postData;
-      res.end(JSON.stringify(resData));
-    });
-  }
-});
 
-server.listen(8000);
+  // 处理 user 路由
+  const userData = handleUserRouter(req, res);
+  if (userData) {
+    res.end(JSON.stringify(userData));
+    return;
+  }
+
+  // 未找到路由，返回 404
+  res.writeHead(404, { "Content-type": "text/plain" });
+  res.write("404 not found\n");
+  res.end();
+};
+
+module.exports = serverHandle;
+
+// process.env.NODE_ENV
